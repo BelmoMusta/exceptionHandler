233b9263407c7c1a206e10317bbc466e89874c5a
Your Name <you@example.com>
2018-01-20 17:16:55 +0000

    multiple types of exception to be caught

--- src/test/java/mustabelmo/exception/handler/test/TryCatchTest.java
+++ src/test/java/mustabelmo/exception/handler/test/TryCatchTest.java
@@ -12,22 +12,6 @@ public class TryCatchTest extends TestCase {
 
     public static final String MESSAGE = "here is your exception";
 
-    @Test
-    public void testUniqueTryCatchBlock() {
-        final boolean[] check = {false};
-        TryCatcher tryCatcher = new TryCatcher();
-        tryCatcher.tryBlock(() -> {
-                    throw new Exception(MESSAGE);
-                },
-                throwable -> {
-                    throwable.printStackTrace();
-                    check[0] = throwable.getMessage().equals(MESSAGE);
-                });
-        assertEquals(check[0], true);
-    }
-
-
-    @Test
     public void testUniqueTryCatchBlockWithConstructor() {
         final boolean[] check = {false};
         TryBlock tryBlock = () -> {
@@ -39,22 +23,48 @@ public class TryCatchTest extends TestCase {
         };
 
         TryCatcher tryCatcher = new TryCatcher(tryBlock, catchBlockBlock);
+        tryCatcher.execute();
         assertEquals(check[0], true);
     }
 
-    @Test
+
     public void testWithoutCatchBlock() {
         final boolean[] check = {true};
 
         TryBlock tryBlock = () -> {
             //  don't throw any exception from here
-
         };
         CatchBlock catchBlockBlock = throwable -> {
             check[0] = false;
             throwable.printStackTrace();
         };
         TryCatcher tryCatcher = new TryCatcher(tryBlock, catchBlockBlock);
+        tryCatcher.execute();
         assertEquals(check[0], true);
+    }
+
+    public void testMultipleExceptions() {
+        final boolean[] check = {true};
+
+        TryBlock tryBlock = () -> {
+            if (check[0]) throw new IllegalArgumentException("ILLEGAL ARGUMENT");
+            else throw new NullPointerException("A DUMMY NULL POINTER EXCEPTION ");
+        };
+        CatchBlock illegalArgBlock = throwable -> {
+            check[0] = false;
+            throwable.printStackTrace();
+        };
+
+        CatchBlock nullPointerBlock = throwable -> {
+            check[0] = false;
+            throwable.printStackTrace();
+        };
+        TryCatcher tryCatcher = new TryCatcher(tryBlock);
+
+        tryCatcher.when(NullPointerException.class, nullPointerBlock)
+                .when(IllegalArgumentException.class, illegalArgBlock)
+                .execute();
+
+        assertEquals(check[0], false);
     }
 }
